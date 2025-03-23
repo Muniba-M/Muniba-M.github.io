46fc4cb70c2390bd2d6f39194214984c82f74cac
Muniba <maudodimuniba@gmail.com>
2025-03-23 16:37:05 -0400

    darkening and lightening the image
    in main.js, including a handler that will lighten/darken the button depending current value of const btnClass = btn.getAttribute('class'); i.e. if it is "dark" lighten the image, if it is "light" (or anything other than "dark") lighten the image

--- main2.js
+++ main2.js
@@ -29,3 +29,17 @@ for (const filename of filenames) {
 }
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click", ()=> {
+    const btnClass = btn.getAttribute('Class');
+    if (btnClass === 'dark') {
+        btn.setAttribute("class", 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute("class", 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+})
+
+
