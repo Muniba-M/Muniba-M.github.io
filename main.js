53011bfbb6a9781c21e2f8df7fcb8e31f480e7f3
Muniba <maudodimuniba@gmail.com>
2025-03-23 02:20:45 -0400

    converting 300lb to stone and 94 fahrenhiet to centigrade
    in result function(), if the "uk" radio input is selected, converts the 300lb to its equivalent in "stones" and 94 degrees fahrenhiet to its equivalent in centigrade using the equation celsius = (fahrenheit-32) * 5 / 9

--- main.js
+++ main.js
@@ -29,12 +29,12 @@ function result() {
   if(customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replaceAll("Bob", name);
-    
+
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300 * 0.07142857) + 'Stone';
+    const temperature =  Math.round((94 - 32) * 5/9) + 'Centigrade';
 
   }
 
