9b9da87947802e3613387595c4077bc954a173ab
Muniba <maudodimuniba@gmail.com>
2025-04-03 17:25:22 -0400

    update the collisiondetect code

--- main3_finished.js
+++ main3_finished.js
@@ -77,18 +77,18 @@ class Ball {
   }
 
   collisionDetect() {
-        for (const ball of balls) {
-            if (!(this === ball) && ball.exists) {
-                const dx = this.x - ball.x;
-                const dy = this.y - ball.y;
-                const distance = Math.sqrt(dx * dx + dy * dy);
-  
-                if (distance < this.size + ball.size) {
-                    ball.color = this.color = randomRGB();
-                }
-            }
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exsits) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
         }
-    }  
+      }
+    }
+  }
 }
 
 const balls = [];
